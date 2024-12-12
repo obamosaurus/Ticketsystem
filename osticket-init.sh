e75f6a50fdbd1292c1ea0ef6bfc9696e6a0ed629
Blåhaj Enjoyer <80923387+obamosaurus@users.noreply.github.com>
2024-12-12 10:15:49 +0000

    Anpassungen an php installation, sodass keine benutzereingabe erfordert wird Notiz für vorgehen nächste woche erstellt im README osticket automatisch unzippen und in apache folder moven

--- osticket-init.sh
+++ osticket-init.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+#install unzip
 sudo apt install -y unzip
+
+#unzip and move osTicket
 unzip osTicket-v1.18.1.zip
+mkdir -p /var/www/html/osTicket
 mv osTicket-v1.18.1 /var/www/html/osTicket
+
+#ChangeOwner to www-data
 chown -R www-data:www-data /var/www/html/osTicket
--- php-init.sh
+++ php-init.sh
@@ -2,5 +2,5 @@
 sudo apt update
 sudp apt upgrade
 
-sudo apt install php libapache2-mod-php
-sudo apt install php-mysql
+sudo apt install -y php libapache2-mod-php
+sudo apt install -y php-mysql
