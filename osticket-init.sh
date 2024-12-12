5634e42b11183b76622b99b7c6282177e66db501
Blåhaj Enjoyer <80923387+obamosaurus@users.noreply.github.com>
2024-12-12 09:32:51 +0000

    osTicket wird absofort von userem GitHub heruntergeladen

--- osticket-init.sh
+++ osticket-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://github.com/osTicket/osTicket/releases/download/v1.18.1/osTicket-v1.18.1.zip
+
 unzip osTicket-v1.18.1.zip
 mv osTicket-v1.18.1 /var/www/html/osTicket
 chown -R www-data:www-data /var/www/html/osTicket
