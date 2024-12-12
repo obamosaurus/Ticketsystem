502b1fb545d86061d280a9819edaed321b46071a
schlauerlux <flavio.bianchi@edu.gbssg.ch>
2024-12-12 10:05:59 +0100

    änderung osticket installation file

--- osticket-init.sh
+++ osticket-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://github.com/osTicket/osTicket/releases/download/v1.17.1/osTicket-v1.17.1.zip
-unzip osTicket-v1.17.1.zip
-mv osTicket-v1.17.1 /var/www/html/osTicket
+wget https://github.com/osTicket/osTicket/releases/download/v1.18.17/osTicket-v1.18.17.zip
+unzip osTicket-v1.18.17.zip
+mv osTicket-v1.18.17 /var/www/html/osTicket
 chown -R www-data:www-data /var/www/html/osTicket
