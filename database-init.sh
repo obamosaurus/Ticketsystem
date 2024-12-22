1255fd70b95c6eae530403dae33941879fe4b625
Blåhaj Enjoyer <80923387+obamosaurus@users.noreply.github.com>
2024-12-22 01:51:42 +0100

    Update database-init.sh
    Anpassungen an wesentlichen funktionen des scriptes um benutzereingaben zu vermeiden. keine dialogfelder mehr.
    
    ausserdem wird nun die db erstellt und root user direkt korrekt angelegt mit PW

--- database-init.sh
+++ database-init.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-# Update der Paketliste
-sudo apt update && sudo apt upgrade -y
+# benutzereingaben verhindern
+export DEBIAN_FRONTEND=noninteractive
 
-# Installation von mysql
+# needrestart: services neustarten ohne Daemons popup
+sudo sh -c 'echo "\$nrconf{restart} = '\''a'\'';" > /etc/needrestart/conf.d/99-restart-auto.conf'
+
+# apt updaten und upgrade
+sudo apt update
+sudo apt upgrade -y
+
+# MySQL installieren
 sudo apt install -y mysql-server
 
-# mysql Server starten 
+# MySQL service starten und aktivieren
 sudo systemctl start mysql
 sudo systemctl enable mysql
 
-# mysql anmelden und database erstellen
-sudo mysql
-create database osTicketDB; 
+# DB erstellen und root user passwort setzen
+sudo mysql -e "CREATE DATABASE osTicketDB; \
+               ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Riethuesli>12345!';"
